9dcb74305f37e787f4a861565204fb4f19d7fdec
kseidl <kseidl@student.tgm.ac.at>
2020-12-14 16:29:25 +0100

    JavaDoc

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Stack"},{"p":"<Unnamed>","l":"StackEmptyException"},{"p":"<Unnamed>","l":"StackFullException"},{"p":"<Unnamed>","l":"Test"}]
